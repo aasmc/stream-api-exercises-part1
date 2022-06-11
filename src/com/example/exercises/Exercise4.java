31d4aa1528574aca911f9013a5b1bc157cba9946
aleksas <aasmc@yandex.ru>
2022-06-11 11:32:47 +0300

    Solve exercise 4

--- src/com/example/exercises/Exercise4.java
+++ src/com/example/exercises/Exercise4.java
@@ -2,6 +2,7 @@ package com.example.exercises;
 
 import java.util.Comparator;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.example.dao.CityDao;
 import com.example.dao.CountryDao;
@@ -10,19 +11,23 @@ import com.example.domain.City;
 import com.example.domain.Country;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise4 {
-	private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
-	private static final CityDao cityDao = InMemoryWorldDao.getInstance();
+    private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
+    private static final CityDao cityDao = InMemoryWorldDao.getInstance();
 
-	public static void main(String[] args) {
-		// Find the highest populated capital city
-//		var highPopulatedCapitalCity;
-//
-//		highPopulatedCapitalCity.ifPresent(System.out::println);
-	}
+    public static void main(String[] args) {
+        // Find the highest populated capital city
+        var highPopulatedCapitalCity = countryDao.findAllCountries()
+                .stream()
+                .mapToInt(Country::getCapital)
+                .mapToObj(cityDao::findCityById)
+                .filter(Objects::nonNull)
+                .max(Comparator.comparing(City::getPopulation));
+
+        highPopulatedCapitalCity.ifPresent(System.out::println);
+
+    }
 
 }
