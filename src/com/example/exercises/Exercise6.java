bada3232580163680a53cb53f867b29c941866b5
aleksas <aasmc@yandex.ru>
2022-06-11 11:43:19 +0300

    Solve exercise 6

--- src/com/example/exercises/Exercise6.java
+++ src/com/example/exercises/Exercise6.java
@@ -2,24 +2,26 @@ package com.example.exercises;
 
 import java.util.Comparator;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise6 {
-	private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
+    private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
 
-	public static void main(String[] args) {
-		// Sort the countries by number of their cities in descending order
-		Function<CountryCityCountPair, Integer> countExtracter = CountryCityCountPair::count;
-//		var countriesWithCityCountInDescOrder;
-//
-//		countriesWithCityCountInDescOrder.forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        // Sort the countries by number of their cities in descending order
+        Function<CountryCityCountPair, Integer> countExtracter = CountryCityCountPair::count;
+        var countriesWithCityCountInDescOrder = countryDao.findAllCountries()
+                .stream()
+                .map(country -> new CountryCityCountPair(country, country.getCities().size()))
+                .sorted(Comparator.comparing(countExtracter).reversed());
+
+        countriesWithCityCountInDescOrder.forEach(System.out::println);
+    }
 
 }
