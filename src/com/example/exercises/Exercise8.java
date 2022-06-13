afad2906a71082fa28ed4877687902cfcd3e8882
Alex Alexeyev <aasmc@yandex.ru>
2022-06-13 15:16:15 +0300

    Solve exercise eight

--- src/com/example/exercises/Exercise8.java
+++ src/com/example/exercises/Exercise8.java
@@ -7,17 +7,19 @@ import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise8 {
-	private static final MovieService movieService = InMemoryMovieService.getInstance();
+    private static final MovieService movieService = InMemoryMovieService.getInstance();
 
-	public static void main(String[] args) {
-		// Group the movies by the year and list them
-//		var moviesByYear;
-//		moviesByYear.forEach((year,movies) -> System.out.printf("%d: %s\n",year,movies));
-	}
+    public static void main(String[] args) {
+        // Group the movies by the year and list them
+        var moviesByYear = movieService.findAllMovies()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(Movie::getYear)
+                );
+        moviesByYear.forEach((year, movies) -> System.out.printf("%d: %s\n", year, movies));
+    }
 
 }
