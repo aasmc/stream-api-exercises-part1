9dc65d7415699e6d36f481332df05304790635fc
Alex Alexeyev <aasmc@yandex.ru>
2022-06-13 15:15:01 +0300

    Solve exercise seven

--- src/com/example/exercises/Exercise7.java
+++ src/com/example/exercises/Exercise7.java
@@ -6,19 +6,23 @@ import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise7 {
-	private static final MovieService movieService = InMemoryMovieService.getInstance();
+    private static final MovieService movieService = InMemoryMovieService.getInstance();
 
-	public static void main(String[] args) {
-		// Find the list of movies having the genres "Drama" and "Comedy" only
-		var listOfGenreNames = List.of("Drama", "Comedy");
-//		var listOfGenres = listOfGenreNames.stream().map(movieService::findGenreByName).toList();
-//		var moviesInDramaAndComedyOnly;
-//		moviesInDramaAndComedyOnly.forEach(movie -> System.out.printf("%s %s\n",movie,movie.getGenres()));
-	}
+    public static void main(String[] args) {
+        // Find the list of movies having the genres "Drama" and "Comedy" only
+        var listOfGenreNames = List.of("Drama", "Comedy");
+        var listOfGenres = listOfGenreNames.stream().map(movieService::findGenreByName).toList();
+        var moviesInDramaAndComedyOnly = movieService
+                .findAllMovies()
+                .stream()
+                .filter(movie -> {
+                    return movie.getGenres().size() == 2;
+                })
+                .filter(movie -> movie.getGenres().containsAll(listOfGenres));
+        moviesInDramaAndComedyOnly.forEach(movie -> System.out.printf("%s %s\n", movie, movie.getGenres()));
+    }
 
 }
